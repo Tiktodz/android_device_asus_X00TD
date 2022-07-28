bebbf0ee340be41ff82943383ca1c2659b93ffd9
anomalchik <vidslava@yandex.ru>
2022-07-28 19:50:49 +0530

    X00TD: Switch to new gesture node
    * Use seperate nodes for each supported gestures while at it.
    
    Change-Id: Icc987986a88d8f53f3411523a62dbeadf81530d0
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,8 +128,7 @@ TARGET_BOARD_PLATFORM := sdm660
 TARGET_ENFORCES_QSSI := true
 
 # Power
-TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
