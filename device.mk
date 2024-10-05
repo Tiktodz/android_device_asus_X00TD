PixelExperience-Devices/device_asus_X00TD

X00TD: Add touchscreen gestures HIDL HAL
Change-Id: I02380e387f01fe20f984d2e6daa095f2056c03f9

device.mk
@@ -428,6 +428,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.X00TD
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic

touch/service.cpp
@@ -1,0 +1,35 @@
+//
+// Copyright (C) 2022 The LineageOS Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.X00TD"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "TouchscreenGesture.h"
+
+using ::android::OK;
+using ::android::sp;
+
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+
+int main() {
+    sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (gestureService->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}

Tiktodz/android_device_asus_X00TD

Signed-off-by: Tiktodz <dotkit@electrowizard.me>

device.mk
@@ -478,6 +478,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.asus_sdm660
+
 # Thermal configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf

touch/service.cpp
@@ -1,0 +1,35 @@
+//
+// Copyright (C) 2023 The LineageOS Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.asus_sdm660"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "TouchscreenGesture.h"
+
+using ::android::OK;
+using ::android::sp;
+
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+
+int main() {
+    sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (gestureService->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}